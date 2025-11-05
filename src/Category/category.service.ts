2cb4742ced2973e3fe07cf48af3531c05a686d5c
Julia Ovcharenko <samsung2100yuliyu@gmail.com>
2025-11-05 20:58:03 +0200

    created category service

--- src/Category/category.service.ts
+++ src/Category/category.service.ts
@@ -1,4 +1,24 @@
 import { CategoryServiceContract } from "./category.types";
+import { CategoryRepository } from "./category.repository";
 
 
-export const CategoryService: CategoryServiceContract ={}
+export const CategoryService: CategoryServiceContract = {
+    getAllCategories: (take, skip) => {
+        return CategoryRepository.getAllCategories(take, skip)
+    },
+    getCategoryById: (id) => {
+        return CategoryRepository.getCategoryById(id)
+    },
+
+    createCategory: (data) => {
+        return CategoryRepository.createCategory(data)
+    },
+
+    deleteCategory: (id) => {
+        return CategoryRepository.deleteCategory(id)
+    }
+}
+
+
+
+
